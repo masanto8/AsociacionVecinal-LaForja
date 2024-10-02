8ea4e70ca4b2de2e4529e1c9eefc9f1f7d18cf12
masanto8 <marihaa1999@gmail.com>
2024-10-02 17:02:10 +0200

    instalacion de strapi y añadidoc componentes

---
+++ src/app/componentes/paginas/talleres/talleres.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-talleres',
+  standalone: true,
+  imports: [],
+  templateUrl: './talleres.component.html',
+  styleUrl: './talleres.component.css'
+})
+export class TalleresComponent {
+
+}
