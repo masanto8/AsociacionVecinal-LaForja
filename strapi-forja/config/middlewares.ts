c99bcb0dd87b440bb813a423fdd08f4115973c11
masanto8 <marihaa1999@gmail.com>
2024-10-15 21:39:41 +0200

    Arreglo strapi

--- strapi-forja/config/middlewares.ts
+++ strapi-forja/config/middlewares.ts
@@ -2,13 +2,6 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:4200'],  // URL del frontend
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    },
-  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
